1f9de84b1f732cf172af2c03c42ffe337a38c3fb
JENO JIJI <jenojiji001@gmail.com>
2026-02-04 18:15:34 +0530

    1to1-pion:  packets recieved in ontrack

--- 1to1-pion/server/peer.go
+++ 1to1-pion/server/peer.go
@@ -3,13 +3,35 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/gorilla/websocket"
+	"github.com/pion/rtp"
 	"github.com/pion/webrtc/v4"
 )
 
 func NewPeer(client *Client, room *Room) (*webrtc.PeerConnection, error) {
 	pc, err := webrtc.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = pc.AddTransceiverFromKind(
+		webrtc.RTPCodecTypeAudio,
+		webrtc.RTPTransceiverInit{
+			Direction: webrtc.RTPTransceiverDirectionSendrecv,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = pc.AddTransceiverFromKind(
+		webrtc.RTPCodecTypeVideo,
+		webrtc.RTPTransceiverInit{
+			Direction: webrtc.RTPTransceiverDirectionSendrecv,
+		},
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -37,6 +59,26 @@ func NewPeer(client *Client, room *Room) (*webrtc.PeerConnection, error) {
 
 	pc.OnConnectionStateChange(func(pcs webrtc.PeerConnectionState) {
 		fmt.Println("peer connection state changed:", pcs)
+	})
+
+	pc.OnTrack(func(tr *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
+		log.Printf("Track recieved: kind=%s, codec=%s", tr.Kind(), tr.Codec().MimeType)
+		go func() {
+			buf := make([]byte, 1400)
+			rtpPacket := &rtp.Packet{}
+			for {
+				_, _, readErr := tr.Read(buf)
+				if readErr != nil {
+					log.Println("Track read error:", readErr)
+					return
+				}
+				// In a real application, you would forward the RTP packets to another peer or process them.
+
+				fmt.Printf(rtpPacket.String())
+				// fmt.Println(buf[:n])
+			}
+		}()
+
 	})
 	return pc, nil
 }
